27ca6a4d237494c28659ef6f05e8f47d0a049edd
sinku <sininoir@gmail.com>
2022-01-08 19:50:13 +0330

    Refactor: Move getDiscount method

--- java/src/main/java/dojo/supermarket/model/Offer.java
+++ java/src/main/java/dojo/supermarket/model/Offer.java
@@ -29,4 +29,33 @@ public class Offer {
         }
         return numberOfBoughtItemsRequired;
     }
+
+    Discount getDiscount(Product product, double quantity, double unitPrice) {
+        int quantityAsInt = (int) quantity;
+        int numberOfBoughtItemsRequired = this.getNumberOfBoughtItemsRequired();
+        int numberOfApplies = quantityAsInt / numberOfBoughtItemsRequired;
+        Discount discount = null;
+        if (offerType == SpecialOfferType.TenPercentDiscount) {
+            discount = new Discount(product, argument + "% off", -quantity * unitPrice * argument / 100.0);
+        }
+        if (numberOfApplies > 0) {
+            if (offerType == SpecialOfferType.ThreeForTwo) {
+                double discountAmount = quantity * unitPrice - ((numberOfApplies * 2 * unitPrice) + quantityAsInt % 3 * unitPrice);
+                discount = new Discount(product, "3 for 2", -discountAmount);
+            }
+            if (offerType == SpecialOfferType.TwoForAmount) {
+                double pricePerUnit = argument * numberOfApplies;
+                double theTotal = (quantityAsInt % 2) * unitPrice;
+                double total = pricePerUnit + theTotal;
+                double discountN = unitPrice * quantity - total;
+                discount = new Discount(product, "2 for " + argument, -discountN);
+            }
+            if (offerType == SpecialOfferType.FiveForAmount) {
+                double discountTotal = unitPrice * quantity - (argument * numberOfApplies + quantityAsInt % 5 * unitPrice);
+                discount = new Discount(product, numberOfBoughtItemsRequired + " for " + argument, -discountTotal);
+            }
+        }
+
+        return discount;
+    }
 }
